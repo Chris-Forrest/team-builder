97bde48a72622795e635928bd8f4be88c122988c
Chris Forrest <redrider_cr@yahoo.com>
2020-04-21 19:04:16 -0400

    initial values teamlist set and added h1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+const initialTeamList = [{
+  name:'Cody',
+  email:'Cody@cody.com',
+  role: 'Guru'
+
+}]
+
+const initialValues = [{
+  name:'',
+  email:'',
+  role: ''
+}]
+const [teamMembers, setTeamMembers] = useState(initialTeamList);
+
+const [formValues, setFormValues] = useState(initialValues);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header><h1 style={{textAlign:'center'}}>Team Builder App</h1></header>
+      <form>
+      
+      </form>
+      
     </div>
   );
 }
