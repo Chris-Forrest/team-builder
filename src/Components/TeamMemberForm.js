7a2a42fc8067b86da605be039651b96f4e57920d
Chris Forrest <redrider_cr@yahoo.com>
2020-04-21 22:18:00 -0400

    making TeamMemberForm function on that component page

--- src/Components/TeamMemberForm.js
+++ src/Components/TeamMemberForm.js
@@ -1,1 +1,25 @@
 import React from 'react';
+
+
+function TeamMemberForm (props){
+    return(
+        <form>
+        <input
+            name="name"
+            value={props.name}
+            onChange={props.onInputChange}
+          > <label>Email:</label>
+          <input 
+                email="email"
+                value={props.email}
+                onChange={props.onInputChange}>
+                  </input>  
+            <label>Role:</label></input>
+             <input 
+             role="role"
+             value={props.role}
+             onChange={props.onInputChange}>
+             </input>
+            </form>
+    )
+}
